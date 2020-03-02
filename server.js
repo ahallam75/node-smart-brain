2cc4eaedf50aa24caebe3297dee7aa56c2b00f96
ahallam <alexhallam75@gmail.com>
2020-03-02 09:18:25 -0500

    Initial signin setup

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: 123,
+      name: "John Smith",
+      email: "john@email.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 124,
+      name: "Sally Thomas",
+      email: "sally@email.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
   res.send("This is working");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
 
 app.listen(3000, () => {
