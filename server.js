c33509bb34fd9473719a1e0e1dfcebc3fc60778c
ahallam <alexhallam75@gmail.com>
2020-03-02 09:57:27 -0500

    Initial profile setup

--- server.js
+++ server.js
@@ -54,6 +54,20 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id == id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("App is running");
 });
